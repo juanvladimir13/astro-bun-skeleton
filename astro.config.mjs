46913fab053bfece8732335ca48a3dabc787ae02
Juan Vladimir <juanvladimir13@gmail.com>
2025-06-08 18:14:38 -0400

    remove default value

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   env: {
     schema: {
-      API_URL: envField.string({ context: "client", access: "public", default: "example" }),
+      API_URL: envField.string({ context: "client", access: "public" }),
       PORT: envField.number({ context: "client", access: "public", optional: true }),
       API_SECRET: envField.string({ context: "server", access: "secret" }),
     }
