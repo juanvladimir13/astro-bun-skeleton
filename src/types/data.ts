6d56d62eaabfac48c02916534f2a8ed3443228d9
Juan Vladimir <juanvladimir13@gmail.com>
2025-06-08 19:45:10 -0400

    add datatypes *.yml

---
+++ src/types/data.ts
@@ -0,0 +1,9 @@
+import type { IHobbie } from "@datatypes/datatypes";
+
+export const HOBBIES: IHobbie[] = [
+  { id: 1, name: 'Reading', level: 5 },
+  { id: 2, name: 'Gaming', level: 4 },
+  { id: 3, name: 'Cooking', level: 3 },
+  { id: 4, name: 'Hiking', level: 2 },
+  { id: 5, name: 'Photography', level: 1 },
+];
